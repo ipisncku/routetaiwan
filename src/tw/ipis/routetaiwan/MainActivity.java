5e8420a127369a5c1fa135dbab79a4e8fa32a46a
Ethan Yang <ethan.yang@lileesystems.com>
2013-10-11 16:33:51 +0800

    Signed and published, v1.0

--- src/tw/ipis/routetaiwan/MainActivity.java
+++ src/tw/ipis/routetaiwan/MainActivity.java
@@ -3,6 +3,7 @@ package tw.ipis.routetaiwan;
 import java.io.File;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -20,6 +21,26 @@ public class MainActivity extends Activity {
 		if (!folder.exists()) {
 			folder.mkdir();
 		}
+		
+		/* Create shortcut on desktop */
+		Intent shortcutIntent;
+		shortcutIntent = new Intent();
+		shortcutIntent.setComponent(new ComponentName(
+				getApplicationContext().getPackageName(), ".classname"));
+
+		shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		shortcutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+		final Intent putShortCutIntent = new Intent();
+		putShortCutIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT,
+				shortcutIntent);
+
+		// Sets the custom shortcut's title
+		putShortCutIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getResources().getString(R.string.app_name));
+		putShortCutIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,Intent.ShortcutIconResource.fromContext(
+				this, R.drawable.routetaiwan));
+		putShortCutIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+		sendBroadcast(putShortCutIntent);
 	}
 	public void my_favorite(View v) {
 		Intent myfavorite = new Intent(this, myfavorite.class);
@@ -39,7 +60,7 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+//		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
