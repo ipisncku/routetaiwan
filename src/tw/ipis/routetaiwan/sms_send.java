bc660f1ee804675c7b93a044d120b60a0267851b
Ethan Yang <ethan.yang@lileesystems.com>
2013-10-30 21:10:07 +0800

    Enhance performance of the contact search

--- src/tw/ipis/routetaiwan/sms_send.java
+++ src/tw/ipis/routetaiwan/sms_send.java
@@ -13,6 +13,7 @@ import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.ContactsContract;
@@ -31,6 +32,7 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class sms_send extends Activity {
@@ -41,6 +43,7 @@ public class sms_send extends Activity {
 	EditText inputSearch, pos_title;
 	ArrayList<HashMap<String, String>> productList;
 	String title, latlng;
+	private ProgressBar contacts;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -51,45 +54,14 @@ public class sms_send extends Activity {
 		title = Data.getString("title");
 		latlng = Data.getString("latlng");
 
-		String contact[] = get_all_contact();
+		contacts = (ProgressBar)findViewById(R.id.contacts);
+		contacts.setVisibility(ProgressBar.VISIBLE);
+		
+		new get_all_contacts().execute();
 
-		lv = (ListView) findViewById(R.id.all_contacts);
 		inputSearch = (EditText) findViewById(R.id.inputSearch);
 		pos_title = (EditText) findViewById(R.id.point_name);
 		pos_title.setText(title);
-
-		// Adding items to listview
-		adapter = new ArrayAdapter<String>(this, R.layout.contact_list, R.id.contact_name, contact);
-		lv.setAdapter(adapter);
-
-		lv.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-
-				String selectedFromList =(String) (lv.getItemAtPosition(position));
-				inputSearch.setText(selectedFromList.replaceAll("[^+0-9]", ""));
-
-			}
-		});
-
-		inputSearch.addTextChangedListener(new TextWatcher() {
-
-			@Override
-			public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
-				// When user changed the Text
-				sms_send.this.adapter.getFilter().filter(cs);   
-			}
-
-			@Override
-			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
-					int arg3) {
-
-			}
-
-			@Override
-			public void afterTextChanged(Editable arg0) {
-			}
-		});
 
 	}
 
@@ -100,6 +72,7 @@ public class sms_send extends Activity {
 					Toast.LENGTH_LONG).show();
 			return;
 		}
+		
 		String title_name = pos_title.getText().toString();
 
 		final String sms = String.format("rtw,%s,%s", title_name, latlng);
@@ -185,40 +158,82 @@ public class sms_send extends Activity {
 		finish();
 	}
 
-	public String[] get_all_contact() {
-		ArrayList<String> contact = new ArrayList<String>();
-		ContentResolver cr = getContentResolver();
-		Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
-				null, null, null, null);
-		if (cur.getCount() > 0) {
-			while (cur.moveToNext()) {
-				String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
-				String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-				if (Integer.parseInt(cur.getString(
-						cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-					Cursor pCur = cr.query(
-							ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-							null,
-							ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
-							new String[]{id}, null);
-					while (pCur.moveToNext()) {
-						String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+	private class get_all_contacts extends AsyncTask<Void, Void, String[]> {
+		@Override
+		protected String[] doInBackground(Void... params) {
+			ArrayList<String> contact = new ArrayList<String>();
+			ContentResolver cr = getContentResolver();
+			Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+					null, null, null, null);
+			if (cur.getCount() > 0) {
+				while (cur.moveToNext()) {
+					String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+					String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+					if (Integer.parseInt(cur.getString(
+							cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+						Cursor pCur = cr.query(
+								ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+								null,
+								ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+								new String[]{id}, null);
+						while (pCur.moveToNext()) {
+							String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-						contact.add(String.format("%s %s", name, phoneNo));
+							contact.add(String.format("%s %s", name, phoneNo));
+						}
+						pCur.close();
 					}
-					pCur.close();
 				}
 			}
-		}
-		if(contact.size() > 0) {
-			String list[] = new String[contact.size()];
-			for(int i=0; i<contact.size(); i++) {
-				list[i] = contact.get(i);
+			if(contact.size() > 0) {
+				String list[] = new String[contact.size()];
+				for(int i=0; i<contact.size(); i++) {
+					list[i] = contact.get(i);
+				}
+				return list;
 			}
-			return list;
+
+			return new String[0];
 		}
 
-		return new String[0];
+		@Override
+		protected void onPostExecute(String[] contact) {
+			// Adding items to listview
+			contacts.setVisibility(ProgressBar.GONE);
+			lv = (ListView) findViewById(R.id.all_contacts);
+			adapter = new ArrayAdapter<String>(sms_send.this, R.layout.contact_list, R.id.contact_name, contact);
+			lv.setAdapter(adapter);
+
+			lv.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+
+					String selectedFromList =(String) (lv.getItemAtPosition(position));
+					inputSearch.setText(selectedFromList.replaceAll("[^+0-9]", ""));
+
+				}
+			});
+
+			inputSearch.addTextChangedListener(new TextWatcher() {
+
+				@Override
+				public void onTextChanged(CharSequence cs, int arg1, int arg2, int arg3) {
+					// When user changed the Text
+					sms_send.this.adapter.getFilter().filter(cs);   
+				}
+
+				@Override
+				public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+						int arg3) {
+
+				}
+
+				@Override
+				public void afterTextChanged(Editable arg0) {
+				}
+			});
+		}
+
 	}
 
 	@Override
